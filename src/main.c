93b05d643b663ef0f01b9cfde4afded0524e390b
Jacob Evans <Jevan234@students.kennesaw.edu>
2026-04-24 19:20:31 -0400

    Added initial renderer

--- src/main.c
+++ src/main.c
@@ -3,5 +3,25 @@
 
 int main() {
 
+    int image_width = 256;
+    int image_height = 256;
+
+    // print header for ppm rendering
+    printf("P3\n");
+    printf("%d %d 255\n", image_width, image_height); // 255 is color depth
+
+
+    // loops for pixel generation, goes across row, then down a column (like a CRT)
+    for (int i = 0; i < image_height; i ++) {
+        for (int j = 0; j < image_width; j ++) {
+            int red = 255;
+            int blue = 0;
+            int green = 125;
+            printf("%d %d %d ", red, blue, green);
+        }
+        printf("\n");
+    }
+
+
     return 0;
 }
